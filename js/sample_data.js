dead389cda11186261e3d7da1da31d2e841f8007
Nigel Wade <nigeljohnwade@gmail.com>
2016-03-30 13:36:17 +0100

    Add sample files

---
+++ js/sample_data.js
@@ -0,0 +1,23 @@
+var sampleDashboardJSON = {
+    ID: 1,
+    layoutID: 1,
+    widgetIDList: [1]
+}
+
+var sampleLayoutJSON = {
+    ID: 1,
+    name: 'top-corner-radial'
+}
+
+var sampleWidgetJSON = {
+    ID: 1,
+    name: 'variance-to-goal',
+    defaultTitle: 'Variance to Goal'
+}
+
+var sampleWidgetDataJSON = {
+    widgetID: 1,
+    widgetData: [36, 257, 293],
+    widgetUnits: 'units',
+    widgetTitle: 'Widget Sales'
+}
